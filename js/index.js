ab50d45137b4bd8e3865d8f19405aa09407c0df9
nedlarry <lrrchinedu@gmail.com>
2020-11-12 02:12:48 +0100

    some responsiveness

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 
 const url = 'http://data.fixer.io/api/latest?access_key=2f0122b7a0ea478a90811c399aa2d117&symbols=USD,AUD,CAD,PLN,MXN&format=1';
 
-function fetchCurrency () {
-    fetch(url).then(response => response.json()).then(data => console.log(data))
+const currencySlider = document.querySelector('.currency')
+const container = document.querySelector('.currency-slider')
+let rates;
+
+
+async function fetchCurrency () {
+    const data = await fetch(url)
+    return data.json()
 }
 
-window.addEventListener('load', fetchCurrency)
+fetchCurrency().then((d) => {
+    rates = Object.keys(d.rates)
+    rates.forEach((quoteC) =>{
+        createCurrencyElement(d, quoteC)
+    })
+
+}).catch(e => console.log(e))
+
+function createCurrencyElement (obj, arrayvalue) {
+
+    const pQuote = document.createElement('p')
+    const pValue = document.createElement('p')
+    // const pQouteNode = document.createTextNode(`${obj.base}/${arrayvalue}`)
+    // const pValueNode = document.createTextNode(`${obj.rates[arrayvalue]}`)
+    pQuote.append(`${obj.base}/${arrayvalue}`)
+    pValue.append(`${obj.rates[arrayvalue]}`)
+
+    currencySlider.className = "currency"
+
+    currencySlider.append(pQuote, pValue)
+    container.appendChild(currencySlider)
+
+}
+
+
